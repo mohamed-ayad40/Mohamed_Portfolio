a72619f54272cd95c4c42742fd51403e31fc0852
Windows 10 <mohamedayad5600@gmail.com>
2024-11-30 02:19:28 +0200

    Optimization

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://mohamed-portfolio-drab.vercel.app', // Your website's URL
+    generateRobotsTxt: true, // Generates a `robots.txt` file for search engines
+    changefreq: 'weekly', // Frequency of page updates
+    priority: 0.7, // Priority of URLs
+};
